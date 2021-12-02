3081c353937154f237cbcb265a1dbc843aee68d2
Alejandrooooooo <53746661+chipotleyumtum@users.noreply.github.com>
2021-12-02 10:32:27 -0600

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,9 +1,6 @@
 /* -----------------------------------------------
 /* Author : Vincent Garreau  - vincentgarreau.com
 /* MIT license: http://opensource.org/licenses/MIT
-/* Demo / Generator : vincentgarreau.com/particles.js
-/* GitHub : github.com/VincentGarreau/particles.js
-/* How to use? : Check the GitHub README
 /* v2.0.0
 /* ----------------------------------------------- */
 
@@ -33,7 +30,7 @@ var pJS = function(tag_id, params){
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#ff0000'
+          color: '#000000'
         },
         polygon: {
           nb_sides: 5
